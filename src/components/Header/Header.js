58656f079dea6a3c7376b80632cf5a132c7e462a
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-15 00:54:03 +0200

    style: add comments

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,26 +15,31 @@ function Header() {
         {/* список страниц */}
         <nav>
           <ul className='header__list'>
+            {/* WOMEN */}
             <li className='header__element-list'>
               <a className='header__link' href='/women'>
                 <p className='header__link_text'>WOMEN</p>
               </a>
             </li>
+            {/* BECOME A MODEL */}
             <li className='header__element-list'>
               <a className='header__link' href='/become a model'>
                 <p className='header__link_text'>BECOME A MODEL</p>
               </a>
             </li>
+            {/* CONTACTS */}
             <li className='header__element-list'>
               <a className='header__link' href='/contacts'>
                 <p className='header__link_text'>CONTACTS</p>
               </a>
             </li>
+            {/* instagram */}
             <li className='header__element-list'>
               <a className='header__link' href='https://www.instagram.com/kuchuruk_tatyana/'>
                 <img className='header__link_icon' src={instagram} ></img>
               </a>
             </li>
+            {/* telegram */}
             <li className='header__element-list'>
               <a className='header__link' href='#'>
                 <img className='header__link_icon' src={telegram} ></img>
