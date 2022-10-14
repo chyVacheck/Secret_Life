086a055e69973a6188e6b61f32a5bc3d6db2ff4d
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-14 14:37:04 +0200

    refactor: перенести app в отдельную папку компонента

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import Header from '../Header/Header.js';
+import Main from '../Main/Main.js';
+import Footer from '../Footer/Footer.js';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main>
+        Just test
+      </Main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
