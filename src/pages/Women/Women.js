07ebcdd617f6b8b53606b934f9a7cc7c493f1114
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-15 23:01:58 +0200

    add new component Women

--- src/pages/Women/Women.js
+++ src/pages/Women/Women.js
@@ -1,10 +1,28 @@
 import './Women.css';
 
+import { ListOfWomen } from '../../utils/constants.js';
+
+import Title from '../../components/Title/Title.js';
+import Woman from '../../components/Woman/Woman.js';
+
 function Women() {
   return (
     <section className='women'>
-      
-    </section>
+      <Title>
+        Women
+      </Title>
+
+      <ul className='women__grid'>
+        {ListOfWomen.map((item, index) => {
+          return (
+            <li key={index}>
+              <Woman woman={item}></Woman>
+            </li>
+          )
+        }
+        )}
+      </ul>
+    </section >
   );
 }
 
