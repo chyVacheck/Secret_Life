8653ab9aed052cf476417c0331b37bf3f1fa9b5b
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-15 16:03:23 +0200

    feat: сделать подвал

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,10 @@
 import './Footer.css';
+import instagram from './../../images/icon/instagram.svg';
+import telegram from './../../images/icon/telegram.svg';
+import Creator from './../../components/Creator/Creator.js';
+
 const currentData = new Date().getFullYear();
-const YEAR_OF_SITE_CREATION = 2022;
+const YEAR_OF_SITE_CREATION = 2020;
 let dateForFooter = YEAR_OF_SITE_CREATION.toString();
 
 if (YEAR_OF_SITE_CREATION < currentData) {
@@ -11,7 +15,28 @@ function Footer() {
   return (
     <footer className='footer'>
       <section className='footer__container'>
-        <p className='footer__copyright'>© {dateForFooter} Secret Life</p>
+        <div className='footer__column'>
+          <p className='footer__copyright'>© {dateForFooter} Secret Life</p>
+          <Creator />
+        </div>
+
+
+        <nav>
+          <ul className='footer__list'>
+            {/* instagram */}
+            <li className='footer__element-list'>
+              <a className='footer__link' href='https://www.instagram.com/kuchuruk_tatyana/'>
+                <img className='footer__link_icon' src={instagram} ></img>
+              </a>
+            </li>
+            {/* telegram */}
+            <li className='footer__element-list'>
+              <a className='footer__link' href='#'>
+                <img className='footer__link_icon' src={telegram} ></img>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </section>
     </footer>
   );
