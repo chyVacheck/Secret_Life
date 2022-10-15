0bac31a768438b18f3e09fc5d1864f939acef6e2
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-15 23:01:33 +0200

    add constants

---
+++ src/utils/constants.js
@@ -0,0 +1,88 @@
+import photo from '../images/women/first/photo_10.jpg';
+
+export const ListOfWomen = [
+  {
+    mainPhoto: photo,
+    firstName: 'Очень красивая',
+    secondName: 'Фамилия',
+    age: 18,
+    height: 170,
+    weight: 45,
+    parameter: {
+      bust: 90,
+      waist: 60,
+      hips: 90,
+    },
+    nationality: 'Ukrainian'
+  },
+  {
+    mainPhoto: photo,
+    firstName: 'Ани',
+    secondName: 'Лорак',
+    age: 19,
+    height: 165,
+    weight: 42,
+    parameter: {
+      bust: 85,
+      waist: 63,
+      hips: 82,
+    },
+    nationality: 'Ukrainian'
+  },
+  {
+    mainPhoto: photo,
+    firstName: 'Ани',
+    secondName: 'Лорак',
+    age: 19,
+    height: 165,
+    weight: 42,
+    parameter: {
+      bust: 85,
+      waist: 63,
+      hips: 82,
+    },
+    nationality: 'Ukrainian'
+  },
+  {
+    mainPhoto: photo,
+    firstName: 'Ани',
+    secondName: 'Лорак',
+    age: 19,
+    height: 165,
+    weight: 42,
+    parameter: {
+      bust: 85,
+      waist: 63,
+      hips: 82,
+    },
+    nationality: 'Ukrainian'
+  },
+  {
+    mainPhoto: photo,
+    firstName: 'Ани',
+    secondName: 'Лорак',
+    age: 19,
+    height: 165,
+    weight: 42,
+    parameter: {
+      bust: 85,
+      waist: 63,
+      hips: 82,
+    },
+    nationality: 'Ukrainian'
+  },
+  {
+    mainPhoto: photo,
+    firstName: 'Имя',
+    secondName: 'Фамилия',
+    age: 19,
+    height: 165,
+    weight: 42,
+    parameter: {
+      bust: 85,
+      waist: 63,
+      hips: 82,
+    },
+    nationality: 'Ukrainian'
+  },
+]
