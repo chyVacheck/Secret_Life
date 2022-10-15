409147ca30b31a792af5b78bbbf30af49ff5800d
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-15 16:07:51 +0200

    add component Contacts

--- src/pages/Contants/Contacts.js
+++ src/pages/Contants/Contacts.js
@@ -1,11 +1,44 @@
+
+import Title from '../../components/Title/Title.js';
+
+import Link from '../../components/Link/Link';
 import './Contacts.css';
+
+const text = '( SEO and Director )';
 
 function Contacts() {
   return (
     <section className='contacts'>
-      
+      <Title>
+        Contacts
+      </Title>
+
+      <section className='contacts__info'>
+        {/* Татьяна */}
+        <article className='contacts__column'>
+          <h2 className='contacts__title'>Татьяна Кучерук</h2>
+          <p className='contacts__text'>{text}</p>
+          <div className='contacts__email'>
+            <Link src='mailto:kuchuruk87@icloud.com' className='contacts__link'>
+              kuchuruk87@icloud.com
+            </Link>
+          </div>
+        </article>
+        {/* Анна */}
+        <article className='contacts__column'>
+          <h2 className='contacts__title'>Анна Подопригора</h2>
+          <p className='contacts__text'>{text}</p>
+          <div className='contacts__email'>
+            <Link src='mailto:apodoprigora@icloud.com' className='contacts__link'>
+              apodoprigora@icloud.com
+            </Link>
+          </div>
+
+        </article>
+      </section>
+
+
     </section>
   );
 }
-
 export default Contacts;
