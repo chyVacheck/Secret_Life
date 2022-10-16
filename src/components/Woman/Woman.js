ab81786fbc42da3b581f4ed47a0b76f84c175c13
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-16 21:35:00 +0200

    Update Woman.js

--- src/components/Woman/Woman.js
+++ src/components/Woman/Woman.js
@@ -1,26 +1,27 @@
+import { NavLink } from 'react-router-dom';
 import './Woman.css';
 
-function Woman({ woman }) {
+function Woman({ woman, setSelectedWoman }) {
 
   const mainPhoto = woman.mainPhoto;
   const firstName = woman.firstName;
-  const secondName = woman.secondName;
-  const age = woman.age;
-  const height = woman.height;
-  const weight = woman.weight;
-  const parameter = woman.parameter;
-  const nationality = woman.nationality;
+
+  function changeInfo() {
+    setSelectedWoman(woman);
+  }
 
   return (
     <article className='woman'>
       <div className='woman__card'>
-        <img className='woman__main-photo' src={mainPhoto} />
+        <NavLink onClick={changeInfo} className='woman__link' to='/woman'>
+          <img className='woman__main-photo' src={mainPhoto} />
+        </NavLink>
         {/* <ul className='woman__list'>
         </ul> */}
       </div>
 
       <h4 className='woman__title'>
-        {firstName + ' ' + secondName}
+        {firstName}
       </h4>
 
     </article>
