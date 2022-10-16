2a48ba8144ddc9c84e96afd883354338fda08fd8
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-16 21:30:20 +0200

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,16 @@
-import photo from '../images/women/first/photo_10.jpg';
+
+import {
+  Luda_photos,
+  Alina_photos,
+  Tomiris_photos,
+  Nastya_photos
+} from './images.js';
 
 export const ListOfWomen = [
   {
-    mainPhoto: photo,
-    firstName: 'Очень красивая',
-    secondName: 'Фамилия',
-    age: 18,
+    mainPhoto: Luda_photos[0],
+    firstName: 'Luda',
+    age: 25,
     height: 170,
     weight: 45,
     parameter: {
@@ -13,76 +18,81 @@ export const ListOfWomen = [
       waist: 60,
       hips: 90,
     },
+    photos: Luda_photos,
     nationality: 'Ukrainian'
   },
   {
-    mainPhoto: photo,
-    firstName: 'Ани',
-    secondName: 'Лорак',
-    age: 19,
-    height: 165,
-    weight: 42,
+    mainPhoto: Alina_photos[0],
+    firstName: 'Alina',
+    age: 17,
+    height: 169,
+    weight: 49,
     parameter: {
-      bust: 85,
-      waist: 63,
-      hips: 82,
+      bust: 90,
+      waist: 60,
+      hips: 90,
     },
+    photos: Alina_photos,
     nationality: 'Ukrainian'
   },
   {
-    mainPhoto: photo,
-    firstName: 'Ани',
-    secondName: 'Лорак',
-    age: 19,
-    height: 165,
-    weight: 42,
+    mainPhoto: Tomiris_photos[1],
+    firstName: 'Tomiris',
+    age: 17,
+    height: 171,
+    weight: 51,
     parameter: {
-      bust: 85,
-      waist: 63,
-      hips: 82,
+      bust: 84,
+      waist: 64,
+      hips: 93,
     },
-    nationality: 'Ukrainian'
+    photos: Tomiris_photos,
+    nationality: 'Kazakh',
+    instagram: 'lashesakb'
   },
   {
-    mainPhoto: photo,
-    firstName: 'Ани',
-    secondName: 'Лорак',
-    age: 19,
-    height: 165,
-    weight: 42,
+    mainPhoto: Nastya_photos[1],
+    firstName: 'Nastya',
+    age: 18,
+    height: 166,
+    weight: 45, //todo написать реальный вес
     parameter: {
-      bust: 85,
-      waist: 63,
-      hips: 82,
+      bust: 82,
+      waist: 61,
+      hips: 92,
     },
-    nationality: 'Ukrainian'
+    photos: Nastya_photos,
+    nationality: 'Polka',
+    instargam: 'nasti_k6'
   },
   {
-    mainPhoto: photo,
-    firstName: 'Ани',
-    secondName: 'Лорак',
-    age: 19,
-    height: 165,
-    weight: 42,
+    mainPhoto: Nastya_photos[1],
+    firstName: 'Nastya',
+    age: 18,
+    height: 166,
+    weight: 45, //todo написать реальный вес
     parameter: {
-      bust: 85,
-      waist: 63,
-      hips: 82,
+      bust: 82,
+      waist: 61,
+      hips: 92,
     },
-    nationality: 'Ukrainian'
+    photos: Nastya_photos,
+    nationality: 'Polka',
+    instargam: 'nasti_k6'
   },
   {
-    mainPhoto: photo,
-    firstName: 'Имя',
-    secondName: 'Фамилия',
-    age: 19,
-    height: 165,
-    weight: 42,
+    mainPhoto: Nastya_photos[0],
+    firstName: 'Nastya',
+    age: 18,
+    height: 166,
+    weight: 45, //todo написать реальный вес
     parameter: {
-      bust: 85,
-      waist: 63,
-      hips: 82,
+      bust: 82,
+      waist: 61,
+      hips: 92,
     },
-    nationality: 'Ukrainian'
-  },
+    photos: Nastya_photos,
+    nationality: 'Polka',
+    instargam: 'nasti_k6'
+  }
 ]
