241487253f5ccf56b7b799e3c813b41b394add0b
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-16 21:34:56 +0200

    edit: поменять логику под constants

--- src/pages/Woman/Woman.js
+++ src/pages/Woman/Woman.js
@@ -5,19 +5,11 @@ import Title from '../../components/Title/Title';
 
 import { ListOfWomen } from '../../utils/constants.js';
 
-import mainPhoto from './../../images/women/first/photo_01.jpg';
-import photo_2 from './../../images/women/first/photo_02.jpg';
-import photo_3 from './../../images/women/first/photo_03.jpg';
-import photo_4 from './../../images/women/first/photo_04.jpg';
-import photo_5 from './../../images/women/first/photo_05.jpg';
-import photo_6 from './../../images/women/first/photo_06.jpg';
-
 
 function Woman({ selectedWoman }) {
   ListOfWomen.map(() => { });
 
   const name = selectedWoman.firstName;
-  const secondName = selectedWoman.secondName;
   const age = selectedWoman.age;
   const height = selectedWoman.height;
   const weight = selectedWoman.weight;
@@ -25,15 +17,12 @@ function Woman({ selectedWoman }) {
   const waist = selectedWoman.parameter.waist;
   const hips = selectedWoman.parameter.hips;
   const nationality = selectedWoman.nationality;
-
-  React.useEffect(() => {
-    console.log();
-  }, [])
+  const instagram = selectedWoman.instagram;
 
   return (
     <section className='page-woman'>
       <Title className='page-woman__title'>
-        {name + ' ' + secondName}
+        {name}
       </Title>
 
       {/* Описание */}
@@ -53,45 +42,59 @@ function Woman({ selectedWoman }) {
           <span>weight:</span>
           <span className='page-woman__meaning'>{weight}</span>
         </div>
-
         {/* BUST */}
         <div>
           <span>bust:</span>
           <span className='page-woman__meaning'>{bust}</span>
         </div>
-
         {/* WAIST */}
         <div>
           <span>waist:</span>
           <span className='page-woman__meaning'>{waist}</span>
         </div>
-
         {/* HIPS */}
         <div>
           <span>hips:</span>
           <span className='page-woman__meaning'>{hips}</span>
         </div>
-
         {/* NATIONALITY */}
         <div>
           <span>nationality:</span>
           <span className='page-woman__meaning'>{nationality}</span>
         </div>
+        {/* INSTAGRAM */}
+        <div>
+          <span>instagram</span>
+          <span className='page-woman__meaning'>{instagram}</span>
+        </div>
         {/* SHOE:38 EYES:BROWN HAIR:BROWN */}
       </article>
-
       {/* фоточки */}
       <article className='page-woman__photos'>
         <article className='page-woman__column'>
-          <img className='page-woman__photo' src={mainPhoto} />
-          <img className='page-woman__photo' src={photo_2} />
-          <img className='page-woman__photo' src={photo_3} />
+          {
+            selectedWoman.photos.map((item, index) => {
+              if (index % 2 === 0) {
+                return (
+                  <img key={index} className='page-woman__photo' src={item} />
+                )
+              }
+            })
+
+          }
         </article>
 
         <article className='page-woman__column'>
-          <img className='page-woman__photo' src={photo_4} />
-          <img className='page-woman__photo' src={photo_5} />
-          <img className='page-woman__photo' src={photo_6} />
+          {
+            selectedWoman.photos.map((item, index) => {
+              if (index % 2 === 1) {
+                return (
+                  <img key={index} className='page-woman__photo' src={item} />
+                )
+              }
+            })
+
+          }
         </article>
       </article>
 
