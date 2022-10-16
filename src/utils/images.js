249a6fbea066cf66335db48716417d62d8400415
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-16 21:30:10 +0200

    Create images.js

---
+++ src/utils/images.js
@@ -0,0 +1,50 @@
+//Luda photos
+import Luda_photo_01 from './../images/women/luda/photo_01.jpg';
+import Luda_photo_02 from './../images/women/luda/photo_02.jpg';
+import Luda_photo_03 from './../images/women/luda/photo_03.jpg';
+import Luda_photo_04 from './../images/women/luda/photo_04.jpg';
+import Luda_photo_05 from './../images/women/luda/photo_05.jpg';
+import Luda_photo_06 from './../images/women/luda/photo_06.jpg';
+import Luda_photo_07 from './../images/women/luda/photo_07.jpg';
+import Luda_photo_08 from './../images/women/luda/photo_08.jpg';
+import Luda_photo_09 from './../images/women/luda/photo_09.jpg';
+import Luda_photo_10 from './../images/women/luda/photo_10.jpg';
+
+//Alina photos
+import Alina_photo_01 from './../images/women/Alina/photo_01.jpg';
+import Alina_photo_02 from './../images/women/Alina/photo_02.jpg';
+import Alina_photo_03 from './../images/women/Alina/photo_03.jpg';
+import Alina_photo_04 from './../images/women/Alina/photo_04.jpg';
+import Alina_photo_05 from './../images/women/Alina/photo_05.jpg';
+import Alina_photo_06 from './../images/women/Alina/photo_06.jpg';
+import Alina_photo_07 from './../images/women/Alina/photo_07.jpg';
+
+//Tomiris photos
+import Tomiris_photo_01 from './../images/women/Tomiris/photo_01.jpg';
+import Tomiris_photo_02 from './../images/women/Tomiris/photo_02.jpg';
+import Tomiris_photo_03 from './../images/women/Tomiris/photo_03.jpg';
+import Tomiris_photo_04 from './../images/women/Tomiris/photo_04.jpg';
+
+//Nastya photos
+import Nastya_photo_01 from './../images/women/Nastya/photo_01.jpg';
+import Nastya_photo_02 from './../images/women/Nastya/photo_02.jpg';
+import Nastya_photo_03 from './../images/women/Nastya/photo_03.jpg';
+import Nastya_photo_04 from './../images/women/Nastya/photo_04.jpg';
+
+
+export const Luda_photos = [
+  Luda_photo_01, Luda_photo_02, Luda_photo_03, Luda_photo_04,
+  Luda_photo_05, Luda_photo_06, Luda_photo_07, Luda_photo_08,
+  Luda_photo_09, Luda_photo_10];
+
+export const Alina_photos = [
+  Alina_photo_01, Alina_photo_02, Alina_photo_03, Alina_photo_04,
+  Alina_photo_05, Alina_photo_06, Alina_photo_07];
+
+export const Tomiris_photos = [
+  Tomiris_photo_01, Tomiris_photo_02,
+  Tomiris_photo_03, Tomiris_photo_04];
+
+export const Nastya_photos = [
+  Nastya_photo_01, Nastya_photo_02,
+  Nastya_photo_03, Nastya_photo_04];
