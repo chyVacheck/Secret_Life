647e7810f4ff335d66c81a14c7280c305ef83a22
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-18 22:49:29 +0200

    add items for live validation

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,6 +6,25 @@ import {
   Nastya_photos
 } from './images.js';
 
+export const form = {
+  inputSelector: '.casting__input',
+  submitButtonSelector: '#casting-button-submit',
+  activeButtonClass: 'casting__submit-button_valid',
+  inactiveButtonClass: 'casting__submit-button_invalid',
+  buttonClass: 'button',
+  errorSelector: '.casting__error-mesage',
+};
+
+export const errorMesages = {
+  tooShort: 'Ввод слишком короткий',
+  tooLong: 'Ввод слишком длинный',
+  typeMismatchUrl: 'Введите ссылку на изображение',
+  valueMissing: 'Осталось заполнить',
+  rangeUnderflow: 'Нужно число по больше',
+  rangeOverflow: 'Нужно число по меньше',
+
+}
+
 export const ListOfWomen = [
   {
     mainPhoto: Luda_photos[0],
@@ -20,7 +39,8 @@ export const ListOfWomen = [
     },
     photos: Luda_photos,
     nationality: 'Ukrainian',
-    instagram: 'Livanishina'
+    instagram: 'Livanishina',
+    text: ''
   },
   {
     mainPhoto: Alina_photos[0],
@@ -35,7 +55,8 @@ export const ListOfWomen = [
     },
     photos: Alina_photos,
     nationality: 'Ukrainian',
-    instagram: 'ahoshton.a'
+    instagram: 'ahoshton.a',
+    text: ''
   },
   {
     mainPhoto: Tomiris_photos[1],
@@ -50,7 +71,8 @@ export const ListOfWomen = [
     },
     photos: Tomiris_photos,
     nationality: 'Kazakh',
-    instagram: 'lashesakb'
+    instagram: 'lashesakb',
+    text: ''
   },
   {
     mainPhoto: Nastya_photos[1],
@@ -65,36 +87,10 @@ export const ListOfWomen = [
     },
     photos: Nastya_photos,
     nationality: 'Polka',
-    instagram: 'nasti_k6'
+    instagram: 'nasti_k6',
+    text: ''
   },
-  {
-    mainPhoto: Nastya_photos[1],
-    firstName: 'Nastya',
-    age: 18,
-    height: 166,
-    weight: 45, //todo написать реальный вес
-    parameter: {
-      bust: 82,
-      waist: 61,
-      hips: 92,
-    },
-    photos: Nastya_photos,
-    nationality: 'Polka',
-    instagram: 'nasti_k6'
-  },
-  {
-    mainPhoto: Nastya_photos[0],
-    firstName: 'Nastya',
-    age: 18,
-    height: 166,
-    weight: 45, //todo написать реальный вес
-    parameter: {
-      bust: 82,
-      waist: 61,
-      hips: 92,
-    },
-    photos: Nastya_photos,
-    nationality: 'Polka',
-    instagram: 'nasti_k6'
-  }
 ]
+
+export const inputValidClass = 'casting__input-valid';
+export const inputInValidClass = 'casting__input-invalid';
